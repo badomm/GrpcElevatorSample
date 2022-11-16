b3438cf5545c7282eee0e0228a4104b5aa8844ed
badomm <1110650+badomm@users.noreply.github.com>
2022-11-16 13:54:04 +0100

    Add Elevator Display

---
+++ ElevatorTerminal/Program.cs
@@ -0,0 +1,35 @@
+﻿using ElevatorGrpc;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+        var channel = GrpcChannel.ForAddress("http://localhost:56592", new GrpcChannelOptions { Credentials = ChannelCredentials.Insecure });
+        var client = new ElevatorService.ElevatorServiceClient(channel);
+        
+        Console.CursorVisible = false;
+        Console.WriteLine($"Starting");
+
+        using (var streaming = client.GetStatus(new Empty()))
+        {
+            var getStatusTask = Task.Run(async () =>
+            {
+                while (await streaming.ResponseStream.MoveNext())
+                {
+                    var statuses = streaming.ResponseStream.Current.ElevatorStatuses;
+                    foreach (var status in statuses)
+                    {
+                        Console.WriteLine(status);
+                    }
+                }
+            });
+
+            while (true) { }
+        };
+    }
+}
